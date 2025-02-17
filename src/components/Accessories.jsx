531e3f2ae3d043cfa8f31aafe9e032d3ce160d86
adyaananyamishra <adyaananyamishra@email.com>
2025-02-17 16:53:13 +0530

    first

--- src/components/Accessories.jsx
+++ src/components/Accessories.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 
-import AHeadphones from "../assets/AHeadphones.jpg";
-import ACharger from "../assets/Acharger.jpg";
-import ACovers from "../assets/Acovers.jpg";
-import APowerbanks from "../assets/APowerbanks.jpg";
-import ATemperglass from "../assets/ATemperglass.jpg";
-import AMobilelens from "../assets/AMobilelens.jpg";
-import AUsbcords from "../assets/AUsbcords.jpg";
-import AMemorychips from "../assets/AMemorychips.jpg";
-import AEarphones from "../assets/AEarphones.jpg";
-
 const images = [
-  AHeadphones,
-  ACharger,
-  ACovers,
-  APowerbanks,
-  ATemperglass,
-  AMobilelens,
-  AUsbcords,
-  AMemorychips,
-  AEarphones,
+  "/assets/AHeadphones.jpg",
+  "/assets/ACharger.jpg",
+  "/assets/ACovers.jpg",
+  "/assets/APowerbanks.jpg",
+  "/assets/ATemperglass.jpg",
+  "/assets/AMobilelens.jpg",
+  "/assets/AUsbcords.jpg",
+  "/assets/AMemorychips.jpg",
+  "/assets/AEarphones.jpg",
 ];
 
 const Accessories = () => {
@@ -30,7 +20,11 @@ const Accessories = () => {
           <div key={index} className="flex-shrink-0 rounded-full">
             <div className="w-32 h-32 rounded-full bg-white p-2 flex items-center justify-center shadow-lg">
               <div className="w-24 h-20 rounded-full overflow-hidden">
-                <img src={src} alt={`Item ${index}`} className="w-full h-full object-cover" />
+                <img
+                  src={src}
+                  alt={`Item ${index}`}
+                  className="w-full h-full object-cover"
+                />
               </div>
             </div>
           </div>
