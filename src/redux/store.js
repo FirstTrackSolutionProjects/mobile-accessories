50637ea0b0e010863cd0af5474f3ffca9fe731bb
adyaananyamishra <adyaananyamishra@email.com>
2025-03-17 14:13:41 +0530

    cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit'
-// import cartReducer from './cartslice/cartslice'
-
-// export default configureStore({
-//   reducer: {
-//     cart : cartReducer
-//   }
-// })
-
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartslice/cartslice";
 
